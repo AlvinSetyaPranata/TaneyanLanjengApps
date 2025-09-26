35c9ccb4a30181b2ea391bf1a954d80501e384ab
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-09-26 18:11:40 +0700

    UPDATE: type cleaning

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import RootLayout from '../layouts/RootLayout'
 import Home from './Home'
 
